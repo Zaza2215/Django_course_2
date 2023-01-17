c1503b4796277dc9eeeba0b1e87f8687ca9d1500
Pasha Kozachenko <pasha22151521@gmail.com>
2023-01-17 11:59:22 +0200

    Add pages and redirect

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ajuc2*2d*s^n__!w81f#l$o^lld*bngrezrk5$4^@ugvvm+#&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- women/views.py
+++ women/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 # Create your views here.
 def index(request):
     return HttpResponse('Page app women.')
+
+
+def categories(request, category_id):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f'<h1>Category {category_id}</h1>')
+
+
+def archive(request, year):
+    if year > 2023:
+        # Http404()
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f'year: {year}')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Page was not found</h1>')

365cbce73650ec3912a3de48fe2aa11c56727c8d
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-02-25 22:57:20 +0200

    10 Admin panel
    * add Category & Women to admin
    * add filters
    * add sorting
    * add fields

--- women/admin.py
+++ women/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from women.models import Women
+from women import models
+
 
 # Register your models here.
-admin.site.register(Women)
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "time_create", "photo", "is_published", "cat")
+    list_display_links = ("id", "title")
+    search_fields = ("title", "content")
+    list_editable = ("is_published",)
+    list_filter = ("is_published", "time_create")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+
+admin.site.register(models.Women, WomenAdmin)
+admin.site.register(models.Category, CategoryAdmin)
--- women/models.py
+++ women/models.py
@@ -5,13 +5,13 @@ from django.urls import reverse
 # Create your models here.
 
 class Women(models.Model):
-    title = models.CharField(max_length=48)
+    title = models.CharField(max_length=48, verbose_name="name")
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Category")
 
     def __str__(self):
         return self.title
@@ -19,9 +19,14 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = "Famous woman"
+        verbose_name_plural = "Famous women"
+        ordering = ["time_create", "title"]
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=48, db_index=True)
+    name = models.CharField(max_length=48, db_index=True, verbose_name="Category")
 
     def __str__(self):
         return self.name

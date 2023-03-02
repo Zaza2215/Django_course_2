651b92be32726f817144e2ad536154bb8534bdaf
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-03-02 16:04:55 +0200

    20 Authentication
    * add page login
    * add authentication
    * add authentication after registration

--- women/forms.py
+++ women/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from . import models
 
@@ -43,8 +43,8 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-input'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
-        }
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label="Login", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- women/urls.py
+++ women/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add-article'),
     path('contact/', contact, name='contact'),
-    path('login/', login, name='login'),
+    path('login/', LoginUser.as_view(), name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('category/<slug:cat_slug>', WomenCategory.as_view(), name='category'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
+    path('logout/', logout_user, name='logout')
 ]
